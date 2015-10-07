9b39896e199c4bf8944d3f2f236c5b6969fcb04b
GonzoDark <lkilde11@student.aau.dk>
2015-10-07 13:12:35 +0200

    Initial Commit

---
+++ Client/Client/SimpleClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class SimpleClient
+    {
+        TcpClient newClient;
+        NetworkStream newNetworkStream;
+        IFormatter formatter;
+        public SimpleClient(string IP, int Port)
+        {
+            
+        }
+    }
+}
